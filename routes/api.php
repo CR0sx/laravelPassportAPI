4bafff7a80a76966c7f15c9681806a90c4ded977
CR0sx <54389090+CR0sx@users.noreply.github.com>
2020-11-20 14:48:12 +0700

    initial config
    all set
    
    need to be done : token not stored in database

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//route for authentication
+Route::post('user/login', [UserController::class, 'login']);
+Route::post('user/register', [UserController::class, 'register']);
+Route::get('user/details', [UserController::class, 'details']);
+Route::post('user/logout', [UserController::class, 'logout']);
+
